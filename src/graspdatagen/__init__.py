06962e44a33ff26e05878a24f79c8611b3a740a5
CrysGate <yuchengong.cs@gmail.com>
2026-09-22 10:17:15 +0800

    add default env parameter

--- src/graspdatagen/__init__.py
+++ src/graspdatagen/__init__.py
@@ -1,1 +1,7 @@
 """USD grasp data production with native Isaac Sim PhysX validation."""
+
+import os
+
+os.environ.setdefault("OMNI_KIT_ACCEPT_EULA", "YES")
+os.environ.setdefault("OPENBLAS_NUM_THREADS", "1")
+os.environ.setdefault("OMP_NUM_THREADS", "8")
